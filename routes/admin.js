55a89ee011d2831b42e4676f15363a961bb1ddc4
lishuang <www.1030415864@qq.com>
2018-04-23 19:36:57 +0800

    0.0

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,6 @@ const admin = require('../controllers/Admin');
 /* GET home page. */
 router.post('/addUser', admin.addUser);
 
-router.post('/deletePay', admin.deletePay);
-
-router.post('/deleteNotice', admin.deleteNotice);
-
 router.post('/deleteUser', admin.deleteUser);
 
 module.exports = router;
